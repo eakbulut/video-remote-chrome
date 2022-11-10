186f0af26fe9c890355ad0768632afab368ad546
Oleksandr Vasylenko <ovasylenko@anaconda.com>
2022-11-10 16:29:05 +0100

    added back/forward

--- content.js
+++ content.js
@@ -18,21 +18,41 @@ const initializeWebSocketConnection = () => {
     if (data.message == "playpause") {
       playPause();
     }
+    if (data.message == "rewind_back") {
+      rewind(-10);
+    }
+    if (data.message == "rewind_forward") {
+      rewind(10);
+    }
+
   };
 };
 
+const isSupported = (hostname) => {
+  console.log(hostname);
+  return (hostname === "www.youtube.com" || hostname === "learning.anaconda.cloud")
+}
 
 const playPause = () => {
+  if (!isSupported(window.location.hostname)) return;
+  
   var video = document.getElementsByTagName("video") [0];
   
-  if (window.location.hostname === "www.youtube.com") {
-    console.log("play/pause youtube video");
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
-    }
-  };
+  console.log("play/pause youtube video");
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const rewind = (seconds) => {
+  if (!isSupported(window.location.hostname)) return;
+
+  var video = document.getElementsByTagName("video") [0];
+  
+  console.log("rewind " + seconds + " seconds");
+  video.currentTime = video.currentTime + seconds;
 };
 
 initializeWebSocketConnection();
